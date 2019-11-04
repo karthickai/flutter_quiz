caee88b91ad2dd0e389fe2c9b2c2967049ac2358
Karthick Pannerselvam <karthickps.ai@gmail.com>
2019-11-04 19:56:20 +0530

    statefull added

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,44 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerButton(){
+    setState(() {
+     questionIndex++; 
+    });
+    print(questionIndex);
+  }
+  var questions = ['Question 1 ?', 'Question 2', 'Question 3'];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz'),
         ),
-        body: Text('Some text element'),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerButton,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
